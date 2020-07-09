04da72a644ecea0b4d569bafbf1b3f96d581d58d
Aaron Lisker <aelisker@gmail.com>
2020-07-08 19:18:53 -0700

    both models added - need testing. first three user routes work

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose');
+
+const emailValidation = function(email) {
+  let regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      index: {
+        unique: true,
+      }
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // https://mongoosejs.com/docs/validation.html
+      // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+      validate: {
+        validator: emailValidation,
+        message: email => `${email.value} is not a valid email address`
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+)
+
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const testRoutes = require('./test-routes');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 router.use('/test', testRoutes);
+router.use('/user', userRoutes);
+router.use('/thought', thoughtRoutes);
 
 module.exports = router;
