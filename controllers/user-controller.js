fbaecb534b135237589e8982aaddf5f7147b6783
Aaron Lisker <aelisker@gmail.com>
2020-07-08 20:02:47 -0700

    all user routes including friends list updates now working

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,6 +7,10 @@ const userContoller = {
         path: 'thoughts',
         select: '-__v'
       })
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
       .select('-__v')
       .sort({ _id: -1 })
       .then(dbUserData => res.json(dbUserData))
@@ -19,6 +23,10 @@ const userContoller = {
     User.findOne({ _id: params.id })
       .populate({
         path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
         select: '-__v'
       })
       .select('-__v')
@@ -58,6 +66,53 @@ const userContoller = {
   },
   deleteUser({ params }, res) {
     User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: `No user found with ID: ${params.id}`});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  addFriend({ params }, res) {
+    console.log(params);
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { friends: params.friendId }},
+      { new: true, runValidators: true }
+    )
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: `No user found with ID: ${params.id}`});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $pull: { friends: params.friendId }},
+      { new: true, runValidators: true }
+    )
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
+      .select('-__v')
       .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: `No user found with ID: ${params.id}`});
